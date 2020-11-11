3e9a40753708aa580b6daaea11eda7f931c4d760
Marcin275143 <73649084+Marcin275143@users.noreply.github.com>
2020-11-11 22:19:12 +0100

    Update ResourceManager.hpp

--- zal/ResourceManager.hpp
+++ zal/ResourceManager.hpp
@@ -4,5 +4,17 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+public:
+    Resource *obiekt;
+    
+    ResourceManager() {};
+    ~ResourceManager() { delete this->obiekt; }
+
+    ResourceManager(const ResourceManager& RM) { *(RM.obiekt) = Resource(); *(RM.obiekt) = *obiekt; }
+
+    ResourceManager& operator=(const ResourceManager& R) { if (&R != this) { delete obiekt;  *(R.obiekt) = Resource(); *(R.obiekt) = *obiekt; return *this; } }
+
+    ResourceManager(ResourceManager&& RP) { obiekt = RP.obiekt; RP.obiekt = nullptr; }
+
+    double get() { return obiekt->get(); }
 };
